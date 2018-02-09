a0e8b8341537ab9b13874e5000bdbfb74ed29030
ananthvadivel1234567890 <35652942+ananthvadivel1234567890@users.noreply.github.com>
2018-02-09 16:52:18 +0530

    Create p num.c

---
+++ p num.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,n;
+    printf("enter the two numbers");
+    scanf("%d%d", &a&b);
+    for(i=a; i<b; i++);
+    {
+        if((i)%<==0)
+        printf("%d\n", i);
+    }
+    }
